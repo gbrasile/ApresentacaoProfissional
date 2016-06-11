64bb3d53f06ff3aace104374fced053f8b507be9
Guilherme Brasile <guilhermebrasile@gmail.com>
2016-06-11 17:36:33 -0300

    Ajuste config bd

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'aula03',
+	'hostname' => 'mysql.hostinger.com.br',
+	'username' => 'u473530032_gui',
+	'password' => '123456',
+	'database' => 'u473530032_gui',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
