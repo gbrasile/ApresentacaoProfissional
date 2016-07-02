9c8065024de7667e71936d574868c56634e2adef
Guilherme Brasile <guilhermebrasile@gmail.com>
2016-07-01 21:59:59 -0300

    Ajuste view sobre

--- application/views/sobre.php
+++ application/views/sobre.php
@@ -13,5 +13,31 @@
         </div>
       </div>
 
+  </div>
+</div>
+
+
+
+<div class="sobre-mobile hidden-md hidden-lg">
+  <div class="row">
+
+      <div class="col-md-10">
+        <div class="panel panel-default panel-sobre">
+          <div class="panel-body">
+            <img class="foto-eu hidden-lg hidden-md" width="80" height="96" align="center"  src="<?php echo base_url() . 'assets/img/fotos/eu2.jpg' ?>">
+            <img class="foto-eu hidden-sm hidden-xs" width="80" height="96" align="left"  src="<?php echo base_url() . 'assets/img/fotos/eu2.jpg' ?>">
+            <h2 align="center"><u>SOBRE</u></h2><br><br>
+              <? echo $conteudo; ?>
+          </div>
+        </div>
+      </div>
+
+  </div>
+</div>
+
+
+    </div>
+</div>
+
 
     
