90e90127d9fe38feff4d768147d7fd725bcd2823
Guilherme Brasile <guilhermebrasile@gmail.com>
2016-07-01 22:18:31 -0300

    voltando view login

--- application/views/login.php
+++ application/views/login.php
@@ -1,4 +1,4 @@
-<div class="sobre-desktop hidden-sm hidden-xs">
+<div class="sobre-desktop">
   <div class="row">
 
       <div class="col-md-10">
@@ -43,46 +43,3 @@
     </div>
 </div>
 
-
-<div class="sobre-mobile hidden-md hidden-lg">
-  <div class="row">
-
-      <div class="col-md-10">
-        <div class="panel panel-default panel-sobre">
-          <div class="panel-body">
-            <img align="center" width="80" height="80" src="<?php echo base_url() . 'assets/img/icons/login.png' ?>">
-            <h2 align="center"><u>LOGIN</u></h2><br>
-            <?php  
-            echo validation_errors(); ?>
-  
-            <?php echo form_open('login/logar', 'role="form" class="formsignin"'); ?>
-            <!-- <form name="formulario" action="<?php //echo base_url() . 'login/autenticar' ?>" method="post"> -->
-
-            <div class="form-group">
-              <label for="exampleInputEmail1">Usuário:</label>
-              <input type="text" name="usuario" class="form-control" placeholder="Usuario">
-            </div>
-
-            <div class="form-group">
-              <label align="left" for="exampleInputEmail1">Senha:</label>
-              <input type="password" name="senha" class="form-control" placeholder="Senha">
-            </div>
-
-            <!--<button type="submit" value="Enviar" class="btn btn-default">Enviar</button>-->
-            <?php echo form_submit('submit', 'Entrar', 'class="btn btn-default"'); ?>
-            <button type="reset" value="limpar formulario" class="btn btn-default">Limpar</button>
-            <? if(!empty($erro)){ ?>
-              <div class="alert alert-danger" role="alert" style=" margin-top: 10px;"><?= $erro; ?></div>
-             <? 
-              } 
-            ?>
-            
-            <!-- </form> -->
-            <?php form_close(); ?>
-          </div>
-        </div>
-      </div>
-
-
-    </div>
-</div>
